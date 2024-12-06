6ee891061a8aaaa66e631c4ef93d12bc5996aa99
“carrrot-cake” <aya.oksh@gmail.com>
2024-12-06 16:31:51 +0200

    added password restrictions in registerDTo

--- backend/src/auth/dto/RegisterRequestDto.ts
+++ backend/src/auth/dto/RegisterRequestDto.ts
@@ -1,19 +1,26 @@
 import { Role } from "src/enums/role.enum";
-import { IsEmail, IsNotEmpty, IsString, IsOptional, IsEnum} from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, IsOptional, IsEnum, Matches} from "class-validator";
 
 export class RegisterRequestDto {
    
   @IsString()
+  @IsNotEmpty()
   name:string;
 
-  //@IsEmail()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
+  // Password validation: Minimum 8 characters, includes uppercase, lowercase, number, and special character
   @IsString()
+  @IsNotEmpty()
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    { message: "Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, one number, and one special character." }
+  )
   password: string;
 
   @IsEnum(Role)
   role: Role;
   }
 
-//an option to remove/update pfp...
