e8a7838c3ccae0f01c3bfd8295228700488dbe2e
“carrrot-cake” <aya.oksh@gmail.com>
2024-12-07 17:40:28 +0200

    added code for input sanitisation for dtos (commented,test)

--- backend/src/auth/dto/SignInDto.ts
+++ backend/src/auth/dto/SignInDto.ts
@@ -1,11 +1,14 @@
 import { IsEmail, IsNotEmpty, IsString, IsOptional, isEmail } from "class-validator";
+//import { sanitizeInput } from "src/common/utils/sanitize-input"; // Import your sanitization utility
 
 export class SignInDto {
 
     @IsNotEmpty()
     @IsEmail()
+    //@Transform(({ value }) => sanitizeInput(value)) // Sanitize email to prevent script injection
     email:string
 
     @IsNotEmpty()
+    //@Transform(({ value }) => sanitizeInput(value))
     password:string
   }
