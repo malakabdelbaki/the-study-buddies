bc1b2f1b809661f33e67f92f5e300dc0e208126d
“carrrot-cake” <aya.oksh@gmail.com>
2024-12-08 01:49:40 +0200

    Santised input and added some restrictions in SignInDTo

--- backend/src/auth/dto/SignInDto.ts
+++ backend/src/auth/dto/SignInDto.ts
@@ -1,14 +1,17 @@
-import { IsEmail, IsNotEmpty, IsString, IsOptional, isEmail } from "class-validator";
-//import { sanitizeInput } from "src/common/utils/sanitize-input"; // Import your sanitization utility
+import { IsEmail, IsNotEmpty, IsString, IsOptional, isEmail, MaxLength } from "class-validator";
+import { Transform } from "class-transformer";
+import { sanitizeInput } from "src/common/utils/sanitise"; // Import your sanitization utility
 
 export class SignInDto {
 
     @IsNotEmpty()
     @IsEmail()
-    //@Transform(({ value }) => sanitizeInput(value)) // Sanitize email to prevent script injection
+    @MaxLength(50, { message: "Email must not exceed 50 characters." }) // Restrict email length
+    @Transform(({ value }) => sanitizeInput(value)) // Sanitize email to prevent script injection
     email:string
 
     @IsNotEmpty()
-    //@Transform(({ value }) => sanitizeInput(value))
+    @MaxLength(128, { message: "Password must not exceed 128 characters." }) // Restrict password length
+    @Transform(({ value }) => sanitizeInput(value))
     password:string
   }
