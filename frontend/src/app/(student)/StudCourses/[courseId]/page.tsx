a90581617d5de2ca0f6d3895ef7a003ca95c84ed
“carrrot-cake” <aya.oksh@gmail.com>
2024-12-23 01:09:43 +0200

    added authorisation hooks to certain pages

--- frontend/src/app/(student)/StudCourses/[courseId]/page.tsx
+++ frontend/src/app/(student)/StudCourses/[courseId]/page.tsx
@@ -10,8 +10,10 @@ import ModuleCard from "../../../../components/course/general/moduleCard";
 import ForumPreview from "@/components/forum/ForumPreview";
 import { createModule } from "../../../api/courses/instructor/moduleRoute";
 import { Types } from "mongoose";
+import { useAuthorization } from "@/hooks/useAuthorization";
 
 const CourseDetails = ({ params }: { params: Promise<{ courseId: string }> }) => {
+  useAuthorization(['student'])
   const [course, setCourse] = useState<Course>();
   const [modules, setModules] = useState<Module[]>();
   const [Instructor,setInstructor] = useState<User>();
